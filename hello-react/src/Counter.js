a24ac43bac6299fad3e56e14a7a5983120ee4702
Dongnyeok Shin <dn.shin98@gmail.com>
2021-07-04 13:44:35 +0900

    [study] Event Practice

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,25 +1,22 @@
-import { render } from '@testing-library/react';
+import { div } from 'prelude-ls';
 import React, {Component} from 'react';
 
 class Counter extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            number: 0
-        };
+    state = {
+        number: 0, 
+        fixedNumber: 0
     }
     render() {
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state;
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
-                onClick={() => {
+                onClick={()=>{
                     this.setState({number: number+1});
                 }}
-                >
-                    +1
-                </button>
+                >+1</button>
             </div>
         );
     }
